568fb50439c2946dbf97f12e5afbb761e307f1b3
Maurice <maurices@stanford.edu>
2017-04-01 13:35:44 -0700

    added function to look up matrix values

--- frodo/src/lib.rs
+++ frodo/src/lib.rs
@@ -1,3 +1,5 @@
+//the fim functions assume that the size of the matrix is a multiply of 4 in order to reduce number of aes calls.
+
 #![feature(asm,concat_idents,const_fn)]
 #![no_std]
 #![feature(type_ascription)]
@@ -126,6 +128,45 @@ pub fn gen_n_row(key128:&[u32; 4], A:&mut[u32; (n) as usize ], irow:u32){
         
 }
 
+//////////////////////////////////////////////////////
+
+
+pub fn gen_dim_row(key128:&[u32; 4], A:&mut[u32; (n) as usize ], irow:u32, nrow:u32){
+
+    for i in 0..nrow/4{
+        unsafe {
+            let aesa_temp = &sam4l::aesa::AES_dev_inst;
+            aesa_temp.aes_get_config_defaults();
+            aesa_temp.aes_set_enable();
+            aesa_temp.aes_set_new_message();
+            aesa_temp.aes_write_key(&key128);
+            
+            let  plain_text:[u32; 4] = [ 4*i+irow*nrow, 4*i+1+irow*nrow,4*i+2+irow*nrow,4*i+3+irow*nrow];
+            aesa_temp.aes_write_input_data(plain_text[0]);
+            aesa_temp.aes_write_input_data(plain_text[1]);
+            aesa_temp.aes_write_input_data(plain_text[2]);
+            aesa_temp.aes_write_input_data(plain_text[3]);
+
+           
+            
+            while aesa_temp.aes_done()!= 1 {
+            }
+            A[ (4*i) as usize ]  = aesa_temp.aes_read_output_data();
+            A[ (4*i+1) as usize ]  = aesa_temp.aes_read_output_data();
+            A[ (4*i+2) as usize ]  = aesa_temp.aes_read_output_data();
+            A[ (4*i+3) as usize ]  = aesa_temp.aes_read_output_data();
+        
+        }
+    
+
+    
+    }
+        
+}
+
+
+///////////////////////////////////////////////////////
+
 
 
 pub fn gen_n_col(key128:&[u32; 4], A:&mut[u32; (n) as usize ], icol:u32){
@@ -164,7 +205,89 @@ pub fn gen_n_col(key128:&[u32; 4], A:&mut[u32; (n) as usize ], icol:u32){
         
 }
 
+
+
+/////////////////////////////////////
+
+pub fn gen_dim_col(key128:&[u32; 4], A:&mut[u32; (n) as usize ], icol:u32, ncol:u32){
+
+    for i in 0..ncol{
+        unsafe {
+            let aesa_temp = &sam4l::aesa::AES_dev_inst;
+            aesa_temp.aes_get_config_defaults();
+            aesa_temp.aes_set_enable();
+            aesa_temp.aes_set_new_message();
+            aesa_temp.aes_write_key(&key128);
+            
+            let block_icol=icol-icol%4;
+            let  plain_text:[u32; 4] = [ i*ncol+block_icol, i*ncol+block_icol+1, i*ncol+block_icol+2, i*ncol+block_icol+3 ];
+            aesa_temp.aes_write_input_data(plain_text[0]);
+            aesa_temp.aes_write_input_data(plain_text[1]);
+            aesa_temp.aes_write_input_data(plain_text[2]);
+            aesa_temp.aes_write_input_data(plain_text[3]);
+
+           
+            
+            while aesa_temp.aes_done()!= 1 {
+            }
+            let zero:u32 = aesa_temp.aes_read_output_data();
+            let one:u32  = aesa_temp.aes_read_output_data();
+            let two:u32  = aesa_temp.aes_read_output_data();
+            let three:u32  = aesa_temp.aes_read_output_data();
+            A[ i as usize ] = equal(0,icol%4)*zero+equal(1,icol%4)*one+equal(2,icol%4)*two+equal(3,icol%4)*three;
+            //A[ i as usize ]=one;
+        
+        }
+    
+
+    
+    }
+       
+        
+}
+
+///////////////////////////////////////////////////////
+
+
+pub fn gen_dim_value(key128:&[u32; 4], A:&mut[u32; (n) as usize ], irow:u32, nrow:u32, icol:u32) -> u32{
+    
+
+    unsafe {
+        let aesa_temp = &sam4l::aesa::AES_dev_inst;
+        aesa_temp.aes_get_config_defaults();
+        aesa_temp.aes_set_enable();
+        aesa_temp.aes_set_new_message();
+        aesa_temp.aes_write_key(&key128);
+        
+        
+        let block_icol=icol-icol%4;
+        let  plain_text:[u32; 4] = [ block_icol+irow*nrow, block_icol+1+irow*nrow,block_icol+2+irow*nrow,block_icol+3+irow*nrow];
+        aesa_temp.aes_write_input_data(plain_text[0]);
+        aesa_temp.aes_write_input_data(plain_text[1]);
+        aesa_temp.aes_write_input_data(plain_text[2]);
+        aesa_temp.aes_write_input_data(plain_text[3]);
+
+       
+        
+        while aesa_temp.aes_done()!= 1 {
+        }
+        let zero:u32 = aesa_temp.aes_read_output_data();
+        let one:u32  = aesa_temp.aes_read_output_data();
+        let two:u32  = aesa_temp.aes_read_output_data();
+        let three:u32  = aesa_temp.aes_read_output_data();
+        let output:u32 = equal(0,icol%4)*zero+equal(1,icol%4)*one+equal(2,icol%4)*two+equal(3,icol%4)*three;
+        return output;
+    
+    }
+
+
+    
+    
+        
+}
+////////////////////////////////////////////
 //if this returns 0, then is means that the diagonal is the same for row and col, the  rest are not tested!!!! so be warned
+//now used for matrix index lookup
 pub fn colvrow_test()-> u32{
     let mut output:u32=0;
     let  key128:[u32; 4] = [0x16157e2b,0xa6d2ae28,0x8815f7ab,0x3c4fcf09];
@@ -173,11 +296,12 @@ pub fn colvrow_test()-> u32{
     //let mut col:[u32;(n) as usize]=[0; (n) as usize];
     
     let mut value:u32=0;
+    let irow:u32=0;
     for i in 0..n{
-        gen_n_row(&key128, &mut row,i as u32);
+        gen_dim_row(&key128, &mut row,irow as u32, n);
         //output=row[(n-1) as usize];
         value=row[i as usize];
-        gen_n_col(&key128, &mut row,i as u32);
+        gen_dim_value(&key128, &mut row,irow as u32, n,n);
         if (value !=row[i as usize]){
             output=output+1;
         }
@@ -190,6 +314,10 @@ pub fn colvrow_test()-> u32{
     return output;
 
 }
+
+/////////////////////////////////////////////////////////
+
+
 
 
 
